f4958bc0872214da7f3c7cf9645cf32565ffca63
Vanshaj Poonia <Vanshajtheunique@gmail.com>
2022-01-08 00:02:26 +0530

    initial commit

---
+++ src/resources/js/license.js
@@ -0,0 +1,14 @@
+/*
+
+Theme: Relacsui – Website UI Kit
+Version: 1.1.0
+Product Page: https://relacsui.hypnosys.in/
+License: MIT
+Author: Vanshaj Poonia
+Author URI: https://vanshajpoonia.com/
+
+---
+
+Copyright 2022 Vanshaj Poonia
+
+*/
